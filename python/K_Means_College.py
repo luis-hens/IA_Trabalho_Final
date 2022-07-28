80e06a11f213be714142ca21e24b00ec531a5160
luis-hens <luishensp@gmail.com>
2022-07-28 10:24:52 -0300

    Finalização Geral

---
+++ python/K_Means_College.py
@@ -0,0 +1,38 @@
+### Importando as bibliotecas ###
+
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+### Leitura dos dados com pandas ###
+
+college = pd.read_csv('go-to-college.csv')
+
+### Convertendo Strings para Inteiros no dataset ###
+
+college['type_school'] = college['type_school'].map({'Academic': 0, 'Vocational': 1})
+college['school_accreditation'] = college['school_accreditation'].map({'A': 0, 'B': 1})
+college['gender'] = college['gender'].map({'Male': 0, 'Female': 1})
+college['interest'] = college['interest'].map({'Not Interested': 0, 'Less Interested': 1, 'Interested': 2, 'Uncertain': 3, 'Very Interested': 4})
+college['residence'] = college['residence'].map({'Urban': 0, 'Rural': 1})
+college['parent_was_in_college'] = college['parent_was_in_college'].map({False: 0, True: 1})
+college['will_go_to_college'] = college['will_go_to_college'].map({False: 0, True: 1})
+
+### Separando as colunas usadas na classificação do dado de entrada na universidade ###
+
+X = college.iloc[:, :-1].values
+Y = college.iloc[:, -1].values
+
+### Aplicação do K-Means
+
+SKMeans = KMeans(n_clusters=2)
+YPredict = SKMeans.fit_predict(X)
+YResult = np.concatenate((YPredict.reshape(len(YPredict), 1), Y.reshape(len(Y), 1)), 1)
+
+### Imprimindo os resultados ###
+
+print('Teste do K-Means')
+cm = confusion_matrix(Y, YPredict)
+print(cm)
+print(accuracy_score(Y, YPredict))
--- python/K_Means_Seattle.py
+++ python/K_Means_Seattle.py
@@ -1,9 +1,30 @@
+### Importando as bibliotecas ###
+
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.metrics import confusion_matrix, accuracy_score
 
-df = pd.read_csv('../seattle-weather.csv')
+### Leitura dos dados com pandas e remoção da coluna date ###
 
-sns.pairplot(df)
-plt.show(block=True)
+seattle = pd.read_csv('seattle-weather.csv')
+seattle = seattle.drop('date', axis=1)
+
+### Separação dos dados e conversão de weather para numéricos ###
+
+seattle['weather'] = seattle['weather'].map({'drizzle': 0, 'fog': 1, 'rain': 2, 'snow': 3, 'sun': 4})
+X = seattle.iloc[:, :-1].values
+Y = seattle.iloc[:, -1].values
+
+### Aplicação do K-Means
+
+SKMeans = KMeans(n_clusters=5)
+YPredict = SKMeans.fit_predict(X)
+YResult = np.concatenate((YPredict.reshape(len(YPredict), 1), Y.reshape(len(Y), 1)), 1)
+
+### Imprimindo os resultados ###
+
+print('Teste do K-Means')
+cm = confusion_matrix(Y, YPredict)
+print(cm)
+print(accuracy_score(Y, YPredict))
