53c07bcf5b0b5d9c654aa9b8c58a48a96543c451
luis-hens <luishensp@gmail.com>
2022-07-28 14:15:16 -0300

    Alterações em K-Means

--- python/K_Means_College.py
+++ python/K_Means_College.py
@@ -17,7 +17,7 @@ college['gender'] = college['gender'].map({'Male': 0, 'Female': 1})
 college['interest'] = college['interest'].map({'Not Interested': 0, 'Less Interested': 1, 'Interested': 2, 'Uncertain': 3, 'Very Interested': 4})
 college['residence'] = college['residence'].map({'Urban': 0, 'Rural': 1})
 college['parent_was_in_college'] = college['parent_was_in_college'].map({False: 0, True: 1})
-college['will_go_to_college'] = college['will_go_to_college'].map({False: 0, True: 1})
+college['will_go_to_college'] = college['will_go_to_college'].map({False: 1, True: 0})
 
 ### Separando as colunas usadas na classificação do dado de entrada na universidade ###
 
@@ -26,13 +26,20 @@ Y = college.iloc[:, -1].values
 
 ### Aplicação do K-Means
 
-SKMeans = KMeans(n_clusters=2)
-YPredict = SKMeans.fit_predict(X)
-YResult = np.concatenate((YPredict.reshape(len(YPredict), 1), Y.reshape(len(Y), 1)), 1)
+for i in range(30):
+    SKMeans = KMeans(n_clusters=5, random_state=i)
+    YPredict = SKMeans.fit_predict(X)
+    YResult = np.concatenate((YPredict.reshape(len(YPredict),1), Y.reshape(len(Y),1)),1)
+    SKMeans.fit(X)
+    college['k-means'] = SKMeans.labels_
 
-### Imprimindo os resultados ###
+    ### Imprimindo tabela e os resultados (Note que o resultado pode estar incorreto dado que nem sempre o K-Means
+    # Acertará o  número correto do cluster). A matriz de confusão foi omitida para melhor legibilidade dos dados ###
 
-print('Teste do K-Means')
-cm = confusion_matrix(Y, YPredict)
-print(cm)
-print(accuracy_score(Y, YPredict))
+    print('Teste do K-Means {}'.format(i+1))
+    cm = confusion_matrix(Y, YPredict)
+    # print(cm)
+    print(accuracy_score(Y, YPredict))
+    
+print("\nDataset")
+print(college)
--- python/K_Means_Seattle.py
+++ python/K_Means_Seattle.py
@@ -18,13 +18,20 @@ Y = seattle.iloc[:, -1].values
 
 ### Aplicação do K-Means
 
-SKMeans = KMeans(n_clusters=5)
-YPredict = SKMeans.fit_predict(X)
-YResult = np.concatenate((YPredict.reshape(len(YPredict), 1), Y.reshape(len(Y), 1)), 1)
+for i in range(30):
+    SKMeans = KMeans(n_clusters=5, random_state=i)
+    YPredict = SKMeans.fit_predict(X)
+    YResult = np.concatenate((YPredict.reshape(len(YPredict),1), Y.reshape(len(Y),1)),1)
+    SKMeans.fit(X)
+    seattle['k-means'] = SKMeans.labels_
 
-### Imprimindo os resultados ###
+    ### Imprimindo tabela e os resultados (Note que o resultado pode estar incorreto dado que nem sempre o K-Means
+    # Acertará o  número correto do cluster). A matriz de confusão foi omitida para melhor legibilidade dos dados ###
 
-print('Teste do K-Means')
-cm = confusion_matrix(Y, YPredict)
-print(cm)
-print(accuracy_score(Y, YPredict))
+    print('Teste do K-Means {}'.format(i+1))
+    cm = confusion_matrix(Y, YPredict)
+    # print(cm)
+    print(accuracy_score(Y, YPredict))
+    
+print("\nDataset")
+print(seattle)
